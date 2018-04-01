703345c9e758aa4aad61cf9f4ad3cff045c3419e
vksmanit <vksmanit@gmail.com>
2018-04-01 20:42:32 +0530

    Committed with automated hybrid_analysis_equation generation and mcg solver : Working Fine

---
+++ nal_nbk_hybrid_analysis_equation.m
@@ -0,0 +1,55 @@
+function [A, B, mcg_index] = nal_nbk_hybrid_analysis_equation(cktnetlist)
+% --------------------------------------------------------------------------------
+% Syntax : [A, B, mcg_index] = nal_nbk_hybrid_analysis_equation(cktnetlist)
+% 
+% This function will return the final equation which is used to solve using 
+% Modified Conjugate Gradient (MCG). 
+% It also return a term mcg_index which is used in modified conjugate gradient.
+% --------------------------------------------------------------------------------
+
+% ---------------------------- written on : Apr 01, 2018 -------------------------
+
+    loop_matrix_for_NBK = nal_nbk_loop_matrix_for_NBK(cktnetlist);
+    incedence_matrix_for_NAL = nal_nbk_incedence_matrix_for_NAL(cktnetlist);
+    incedence_matrix_for_NAL = incedence_matrix_for_NAL(1:end-1, :);
+    L_branch = nal_nbk_L_branch_hybrid_analysis(cktnetlist);
+    K_branch = nal_nbk_K_branch_hybrid_analysis(cktnetlist);
+    partition = nal_nbk_partition(cktnetlist);
+    [GA,EA,JA] = nal_nbk_matrix_for_NAL(cktnetlist);
+    [RB,EB,JB] = nal_nbk_matrix_for_NBK(cktnetlist);
+    ArA= [];
+    Bb = [];
+    ArL = [];
+    BK = [];
+    ArK = [];
+    BL = [];
+    index = 1;
+    for item = partition 
+        if item == 1 
+            ArA = [ArA, incedence_matrix_for_NAL(:,index)];
+        else
+            Bb = [Bb, loop_matrix_for_NBK(:,index)];
+        end
+        index = index + 1;
+    end
+    for item = L_branch
+        ArL = [ArL,incedence_matrix_for_NAL(:,item)];
+        BL = [BL, loop_matrix_for_NBK(:,item)];
+    end
+    for item = K_branch
+        BK = [BK,loop_matrix_for_NBK(:,item)];
+        ArK = [ArK, incedence_matrix_for_NAL(:,item)];
+    end
+
+    A_left_top = ArA * GA * ArA';
+    A_left_bottom = BK * ArK';
+    A_right_top = ArL * BL';
+    A_right_bottom = Bb * RB * Bb' ;
+    A = [ A_left_top, A_right_top ; A_left_bottom, A_right_bottom];
+    B_top = ArA * GA * EA - ArA * JA;
+    B_bottom = Bb * RB * JB - Bb * EB;
+    B = [B_top;B_bottom];
+
+    mcg_index = size(A_left_bottom,2);
+                                       
+end
--- nal_nbk_mcg.m
+++ nal_nbk_mcg.m
@@ -1,32 +1,40 @@
-function x = nal_nbk_mcg(A,b)
+function x = nal_nbk_mcg(A,B,mcg_index)
 % --------------------------------------------------------------------------------------------
-% Syntax :- x = nal_nbk_mcg(A,b) 
+% Syntax : x = nal_nbk_mcg(A,B,mcg_index) 
 %
-% This is modified conjugate gredient method to solve the system of  equation Ax = b,
-% here we have take x = zeros(length(b),1) as our initial guess.
+% This is modified conjugate gredient method to solve the system of  equation Ax = B,
+% here we have take x = zeros(length(B),1) as our initial guess.
 % --------------------------------------------------------------------------------------------
 
 % -------------------------------- written on : Mar 12, 2018 ---------------------------------
-
-    x = zeros(length(b),1);
-    r = b - A*x;
-    p = r;
-    iter = 1;
-    for k = 1:100 %length(b) % since modified CG generate the exact solution to linear system 
-                       % Ax = b in at most n step 
-                       % this is not working with length(b) --> TODO
+   mcg_index_plus_one = mcg_index + 1;
+   x = zeros(length(B),1);
+   col_size_of_A = length(x);
+   r = B - A*x;
+   p = r;
+   iter = 1;
+   for k = 1:100 %length(B) % since modified CG generate the exact solution to linear system 
+                       % Ax = B in at most n step 
+                       % this is not working with length(B) --> TODO
        %iter = iter + 1 ;% variable for iteration
-    Ap = A * p;
-    alpha = (r(1:3)'*p(1:3) - r(4:7)'*p(4:7))/(p(1:3)' * Ap(1:3) - p(4:7)'*Ap(4:7));
-    x = x + alpha * p;
-    % x = x - alpha * p;  % -ve sign is in paper but +ve sign is working
-    rold = r;
-    r = r - alpha * Ap;
-    if ( abs(r-rold) < 1e-6)
-        % iter
-        break;
-    end
-     beta = -(r(1:3)'*Ap(1:3) -r(4:7)'*Ap(4:7))/(p(1:3)' * Ap(1:3) - p(4:7)'*Ap(4:7));
-     p = r + beta * p;
-  %     k
+       Ap = A * p;
+       alpha = (r(1:mcg_index)'*p(1:mcg_index) - r(mcg_index_plus_one:col_size_of_A)'*p(mcg_index_plus_one:col_size_of_A))/(p(1:mcg_index)' * Ap(1:mcg_index) - p(mcg_index_plus_one:col_size_of_A)'*Ap(mcg_index_plus_one:col_size_of_A));
+       x = x + alpha * p;
+      % x = x - alpha * p;  % -ve sign is in paper but +ve sign is working
+       rold = r;
+        r = r - alpha * Ap;
+        temp1 = max(abs(r - rold));
+        if (temp1 < 1e-6)
+       %if ( ((r-rold) < 10^-6) | ((rold -r) < 10 ^-6))
+       %if ( abs(r-rold) < 10^-6) % or (rold -r) < 10 ^-6)
+          % iter
+           break;
+       end
+       beta = -(r(1:mcg_index)'*Ap(1:mcg_index) -r(mcg_index_plus_one:col_size_of_A)'*Ap(mcg_index_plus_one:col_size_of_A))/(p(1:mcg_index)' * Ap(1:mcg_index) - p(mcg_index_plus_one:col_size_of_A)'*Ap(mcg_index_plus_one:col_size_of_A));
+       p = r + beta * p;
+       k;
+   end
+
 end
+
+ 
