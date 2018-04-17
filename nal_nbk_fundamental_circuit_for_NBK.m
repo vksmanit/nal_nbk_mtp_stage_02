86548978b3dcddefd4618885b30235a0346f3b8a
vksmanit <vksmanit@gmail.com>
2018-04-17 16:08:42 +0530

    final working code with nal_nbk_FPGA_ckt_01 and nal_nbk_FPGA_ckt_02

--- nal_nbk_fundamental_circuit_for_NBK.m
+++ nal_nbk_fundamental_circuit_for_NBK.m
@@ -13,10 +13,12 @@ function fundamentalCircuitEdgeIds =  nal_nbk_fundamental_circuit_for_NBK(cktnet
     global edges_for_NBK;
     global node_visited;
     global MyCktIds;
+    global MyOutput;
 
     [tree, link] = nal_nbk_tree_and_link_branch_for_NBK(cktnetlist);
     N = length(cktnetlist.nodenames) + 1;
     [g1_of_tree_of_NBK, edges_for_NBK] = nal_nbk_nodeInfo_with_edge_identity_of_tree_of_NBK(cktnetlist);
+    %edgeId_visited = zeros(length(edges_for_NBK),1)';
     fundamentalCircuitEdgeIds = cell(length(link),1);
     for i = 1 :length(link)
         edge = cktnetlist.elements{link(i)}.nodes;
@@ -34,21 +36,23 @@ function fundamentalCircuitEdgeIds =  nal_nbk_fundamental_circuit_for_NBK(cktnet
             node2 = str2num(cell2mat(node2));
         end
         MyCktIds = [];
-        dfs_to_get_fundamental_circuit(node1,node2);%, MyCktIds);
-        %fundamentalCircuitEdgeIds{i} = [link(i),MyCktIds] ;
-        fundamentalCircuitEdgeIds{i} = [MyCktIds,link(i)] ;
+        dfs_to_get_fundamental_circuit(node1,node2) %, g1_of_tree_of_NBK);%, MyCktIds);
+        fundamentalCircuitEdgeIds{i} = [MyOutput, link(i)] ;
     end
 end
 
-function  dfs_to_get_fundamental_circuit(start_node,end_node)
+function dfs_to_get_fundamental_circuit(start_node, end_node) %, g1_of_tree_of_NBK)
     global edges_for_NBK;
     global node_visited;
+    global MyOutput;
+    global MyCktIds;
     global g1_of_tree_of_NBK;
-    global MyCktIds ;
 
     node_visited(start_node) = 1;
     adjEdgeIdsOfCurrentNode = g1_of_tree_of_NBK{start_node};
-    for edgeId = adjEdgeIdsOfCurrentNode
+    length_of_adjEdgeIdsOfCurrentNode = length(adjEdgeIdsOfCurrentNode);
+    for i = 1:length(adjEdgeIdsOfCurrentNode)
+        edgeId = adjEdgeIdsOfCurrentNode(i);
         edge = edges_for_NBK(edgeId, :);
         otherNode = edge(1,2);
         if (strcmp(otherNode,'gnd'))
@@ -56,29 +60,29 @@ function  dfs_to_get_fundamental_circuit(start_node,end_node)
         else
             otherNode = str2num(cell2mat(edge(1,2)));
         end
+
         if otherNode == start_node 
             otherNode = str2num(cell2mat(edge(1,1)));
             if (isempty(otherNode))
                 otherNode = length(node_visited);
             end
         end
-        
-        if (node_visited(otherNode) == 0)
-            MyCktIds = [MyCktIds, edgeId];
-            if (otherNode == end_node)
-                return 
-            end
-            dfs_to_get_fundamental_circuit(otherNode, end_node);
-        elseif (node_visited(otherNode) == 1)
-            if (length(g1_of_tree_of_NBK{start_node}) == 1) 
-                MyCktIds = [];
-            end
-                continue;
-            if (length(MyCktIds) == 1) 
-                MyCktIds = [];
-            else
-                MyCktIds = MyCktIds(1:end-1);
-            end
+
+        if (otherNode == end_node)
+            MyOutput = [MyCktIds, edgeId];
+            return ;
         end
+        
+        if (node_visited(otherNode) ~= 1)
+            MyCktIds = [MyCktIds, edgeId];
+            dfs_to_get_fundamental_circuit(otherNode, end_node) %, g1_of_tree_of_NBK);  
+        end
+       
+        if ( i == length_of_adjEdgeIdsOfCurrentNode)% & node_visited(otherNode) == 1)
+            MyCktIds = MyCktIds(1:end-1);
+            continue
+        end
+
     end
+        
 end
---
+++ nal_nbk_working_cache_temp.m
@@ -0,0 +1,255 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%555
+function fundamentalCircuitEdgeIds =  nal_nbk_fundamental_circuit_for_NBK(cktnetlist)
+% --------------------------------------------------------------------------------
+% Syntax : fundamentalCircuitEdgeIds = nal_nbk_fundamental_circuit_for_NBK(cktnetlist)
+%
+% This function will return the cell object fundamentalCircuitEdgeIds which is the 
+% EdgeIds of fundamental cirucuit. 
+% The link branch is attached at the end to get compatibility with function 
+% nal_nbk_hybird_analysis_equation(cktnetlist). 
+% --------------------------------------------------------------------------------
+
+% ----------------------- Written On : Apr 14, 2018 ------------------------------
+    global g1_of_tree_of_NBK;
+    global edges_for_NBK;
+    global node_visited;
+    global MyCktIds;
+    global MyOutput;
+
+    [tree, link] = nal_nbk_tree_and_link_branch_for_NBK(cktnetlist);
+    N = length(cktnetlist.nodenames) + 1;
+    [g1_of_tree_of_NBK, edges_for_NBK] = nal_nbk_nodeInfo_with_edge_identity_of_tree_of_NBK(cktnetlist);
+    %edgeId_visited = zeros(length(edges_for_NBK),1)';
+    fundamentalCircuitEdgeIds = cell(length(link),1);
+    for i = 1 :length(link)
+        edge = cktnetlist.elements{link(i)}.nodes;
+        node_visited = zeros(N,1);
+        node1 = edge(1,1);
+        node2 = edge(1,2);
+        if (strcmp(node1, 'gnd'))
+            node1 = N ;
+        else 
+            node1 = str2num(cell2mat(node1));
+        end
+        if (strcmp(node2, 'gnd'))
+            node2 = N ;
+        else 
+            node2 = str2num(cell2mat(node2));
+        end
+        MyCktIds = [];
+        dfs_to_get_fundamental_circuit(node1,node2, g1_of_tree_of_NBK);%, MyCktIds);
+        fundamentalCircuitEdgeIds{i} = [MyOutput, link(i)] ;
+    end
+end
+%
+function MyOutput = dfs_to_get_fundamental_circuit(start_node,end_node,modified_g1_of_tree_of_NBK)
+   %global MyCktIds;
+   %%%%%global g1_of_tree_of_NBK;
+   global edges_for_NBK;
+   global MyOutput;
+   global node_visited;
+   global MyCktIds;
+   %global edgeId_visited;
+
+   node_visited (start_node) = 1;
+   adjEdgeIdsOfCurrentNode = modified_g1_of_tree_of_NBK{start_node};
+   
+   for edgeId = adjEdgeIdsOfCurrentNode
+
+       edge = edges_for_NBK(edgeId, :);
+       otherNode = edge(1,2);
+       if (strcmp(otherNode,'gnd'))
+           otherNode = length(node_visited);
+       else
+           otherNode = str2num(cell2mat(edge(1,2)));
+       end
+
+       if otherNode == start_node 
+           otherNode = str2num(cell2mat(edge(1,1)));
+           if (isempty(otherNode))
+               otherNode = length(node_visited);
+           end
+       end
+
+       if (otherNode == end_node)
+           MyOutput = [MyCktIds,edgeId];
+           return 
+       end
+
+       if (node_visited(otherNode) == 1 & length(modified_g1_of_tree_of_NBK{start_node}) == 1) 
+           if(length(MyCktIds) == 1) 
+               MyCktIds = [];
+           else
+               MyCktIds = MyCktIds(1:end-1); % this will remove an edge when it find a terminated node.
+           end
+           modified_g1_of_tree_of_NBK = remove_coloops(start_node,modified_g1_of_tree_of_NBK);
+          % if (length(modified_g1_of_tree_of_NBK{otherNode}) == 1)
+           %    modified_g1_of_tree_of_NBK = re
+       elseif (node_visited(otherNode) == 0) 
+           MyCktIds = [ MyCktIds, edgeId] 
+           dfs_to_get_fundamental_circuit(otherNode, end_node, modified_g1_of_tree_of_NBK);
+       elseif node_visited(otherNode == 1) 
+           continue;
+       end
+   end
+end
+
+function removed_coloops_g1_of_NBK = remove_coloops(node,g1)
+    global end_node;
+    global edges_for_NBK;
+    global node_visited;
+    edgeId = g1{node};
+    g1{node} = [];
+    edge = edges_for_NBK(edgeId,:);
+    otherNode = edge(1,2);
+    if (strcmp(otherNode,'gnd'))
+        otherNode = length(node_visited);
+    else
+        otherNode = str2num(cell2mat(edge(1,2)));
+    end
+    if otherNode == node 
+        otherNode = str2num(cell2mat(edge(1,1)));
+        if (isempty(otherNode))
+            otherNode = length(node_visited);
+        end
+    end
+    g1{otherNode} = g1{otherNode}(g1{otherNode}~=edgeId);
+    %%%%%%%%%%% working for single coloop branch %%%%%%%%%%%%%%%%%
+    if(length(g1{otherNode}) == 1 & node_visited(otherNode) == 1)
+        removed_coloops_g1_of_NBK = remove_coloops(otherNode,g1);
+    elseif(length(g1{otherNode}) == 1 & node_visited(otherNode) ~= 1)
+        removed_coloops_g1_of_NBK = g1;
+    end
+end
+
+%%%%%%%%%%%%%%we have to write a function to remove coloops %%%%%%%%%%%%
+%
+%
+%unction removed_coloops_g1_of_NBK = remove_coloops(node,g1,end_node)
+%
+%   global edges_for_NBK;
+%   global node_visited;
+%   edgeId = g1{node};
+%
+%   g1{node} = [];
+%
+%   if (node == end_node)
+%       return 
+%   end
+%
+%
+%   edge = edges_for_NBK(edgeId,:);
+%   otherNode = edge(1,2);
+%   
+%   if (strcmp(otherNode,'gnd'))
+%       otherNode = length(node_visited);
+%   else
+%       otherNode = str2num(cell2mat(edge(1,2)));
+%   end
+%   if otherNode == node 
+%       otherNode = str2num(cell2mat(edge(1,1)));
+%       if (isempty(otherNode))
+%           otherNode = length(node_visited);
+%       end
+%   end
+%   g1{otherNode} = g1{otherNode}(g1{otherNode}~=edgeId);
+%%%%%%%%%%% working for single coloop branch %%%%%%%%%%%%%%%%%
+%  if(length(g1{otherNode}) == 1)
+%      removed_coloops_g1_of_NBK = remove_coloops(otherNode,g1);
+%  else
+%      removed_coloops_g1_of_NBK = g1;
+%  end
+%     
+%
+%nd
+%
+
+%
+
+%unction  dfs_to_get_fundamental_circuit(start_node,end_node)
+%   global edges_for_NBK;
+%   global node_visited;
+%   global g1_of_tree_of_NBK;
+%   global MyCktIds ;
+%   global MyOutput;
+%
+%   node_visited(start_node) = 1;
+%   adjEdgeIdsOfCurrentNode = g1_of_tree_of_NBK{start_node};
+%   for edgeId = adjEdgeIdsOfCurrentNode
+%       edge = edges_for_NBK(edgeId, :);
+%       otherNode = edge(1,2);
+%       if (strcmp(otherNode,'gnd'))
+%           otherNode = length(node_visited);
+%       else
+%           otherNode = str2num(cell2mat(edge(1,2)));
+%       end
+%       if otherNode == start_node 
+%           otherNode = str2num(cell2mat(edge(1,1)));
+%           if (isempty(otherNode))
+%               otherNode = length(node_visited);
+%           end
+%       end
+%       
+%       if (node_visited(otherNode) == 0)
+%           MyCktIds = [MyCktIds, edgeId];
+%           if (otherNode == end_node)
+%               MyOutput = MyCktIds;
+%               return 
+%           end
+%           dfs_to_get_fundamental_circuit(otherNode, end_node);
+%       elseif (node_visited(otherNode) == 1)
+%           if (length(g1_of_tree_of_NBK{start_node}) == 1) %%%% remove coloops here
+%               % how to write code here to combine the both the condition
+%              %if (length(MyCktIds) == 1) 
+%              %    MyCktIds = [];
+%              %else
+%              %    MyCktIds = MyCktIds(1:end-1);
+%              %end
+%              MyCktIds = recursive_call_to_remove_coloops(start_node,g1_of_tree_of_NBK);
+%                   
+%           end
+%               continue;
+%           if (length(MyCktIds) == 1) 
+%               MyCktIds = [];
+%           else
+%               MyCktIds = MyCktIds(1:end-1);
+%           end
+%       end
+%   end
+%nd
+%
+%unction MyCktIds =  recursive_call_to_remove_coloops(node, recursive_g1_of_tree_of_NBK);
+%   %global recursive_g1_of_tree_of_NBK;
+%    global g1_of_tree_of_NBK;
+%   global MyCktIds;
+%   global edges_for_NBK;
+%
+%   if (length(recursive_g1_of_tree_of_NBK{node}) ~= 1) 
+%       return ;
+%   end
+%
+%   if (length(recursive_g1_of_tree_of_NBK{node}) == 1) 
+%
+%       edgeId =  recursive_g1_of_tree_of_NBK{node};
+%       edge = edges_for_NBK(edgeId, :);
+%       MyCktIds = MyCktIds(1:end-1);
+%       recursive_g1_of_tree_of_NBK{node} = [];
+%       otherNode = edge(1,2);
+%       
+%       if (strcmp(otherNode,'gnd'))
+%           otherNode = length(nodeVisited);
+%       else
+%           otherNode = str2num(cell2mat(otherNode));
+%       end
+%       if otherNode == node
+%           otherNode = str2num(cell2mat(edge(1,1)));
+%       end
+%
+%       recursive_g1_of_tree_of_NBK{otherNode} = recursive_g1_of_tree_of_NBK{otherNode}(~edgeId);
+%       recursive_call_to_remove_coloops(otherNode, recursive_call_to_remove_coloops);
+%   end
+%   
+%nd
+%
+%   
+%
